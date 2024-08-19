1ee0bd086896e4e54a6a473263ab0ad1b24278d0
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2024-08-19 12:30:18 -0300

    Remove grid and add input file for 2D animation

--- TP2/GameOfLife/analysis/animation2D.py
+++ TP2/GameOfLife/analysis/animation2D.py
@@ -1,23 +1,26 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import json
+import datetime
 import numpy as np
 
 
 # Function to load simulation data from JSON
-def load_simulation_data(json_file):
+def load_json(json_file):
     with open(json_file, 'r') as file:
         data = json.load(file)
     return data
 
-
+# Load animation data
+animation_data = load_json('./input.json')
 # Load simulation data from JSON file
-simulation_data = load_simulation_data('./simulation_results/1.json')
+simulation_data = load_json(animation_data["input_file"])
 
 # Extract grid boundaries and steps
-min_x, max_x = simulation_data['min_x'], simulation_data['max_x']
-min_y, max_y = simulation_data['min_y'], simulation_data['max_y']
-steps = simulation_data['steps']
+border = simulation_data["border"]
+min_x, max_x = border["x.min"], border["x.max"]
+min_y, max_y = border["y.min"], border["y.max"]
+steps = simulation_data['liveCellsForStep']
 
 # Create the plot
 fig, ax = plt.subplots()
@@ -45,9 +48,7 @@ def initialize_rectangles(live_cells):
 # Animation update function
 def update(frame):
     # Get live cells for the current step
-    step_data = steps[frame]
-    live_cells = step_data['live_cells']
-    initialize_rectangles(live_cells)
+    initialize_rectangles(steps[frame])
     return rectangles
 
 
@@ -56,8 +57,8 @@ plt.xlim(min_x - 1, max_x + 1)
 plt.ylim(min_y - 1, max_y + 1)
 
 # Set grid lines to be at every integer
-ax.set_xticks(np.arange(min_x - 1, max_x + 2, 1))
-ax.set_yticks(np.arange(min_y - 1, max_y + 2, 1))
+# ax.set_xticks(np.arange(min_x - 1, max_x + 2, 1))
+# ax.set_yticks(np.arange(min_y - 1, max_y + 2, 1))
 ax.grid(which='both', linestyle='--', linewidth=0.5)
 
 # Add grid, labels, and show plot
@@ -65,9 +66,9 @@ ax.grid(which='both', linestyle='--', linewidth=0.5)
 plt.title('Live Cells Animation')
 
 # Create animation
-ani = animation.FuncAnimation(fig, update, frames=len(steps), interval=500, blit=True)
+ani = animation.FuncAnimation(fig, update, frames=len(steps), interval=animation_data["interval"], blit=True)
 
 # Save and show the animation
 # ani.save(filename="pillow_example.gif", writer="pillow")
-ani.save(filename="./animation/html_example.html", writer="html")
+ani.save(filename=animation_data["output_dir"] + "_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + "/animation.html", writer="html")
 plt.show()
