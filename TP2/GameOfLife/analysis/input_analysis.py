b24752ef454eb4eb48e77f251f8cdde136cfbbd0
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-08-23 14:01:28 -0300

    feat: add scalar_analysis types

--- TP2/GameOfLife/analysis/input_analysis.py
+++ TP2/GameOfLife/analysis/input_analysis.py
@@ -7,26 +7,23 @@ import time
 from matplotlib import pyplot as plt
 
 
-def size_scalar_analysis(output, observation_step) -> float or None:
+def size_scalar_analysis(output, observation_step) -> float:
     live_cells_for_step = output['liveCellsForStep']
     if len(live_cells_for_step) <= observation_step:
-        return None
+        observation_step = len(live_cells_for_step)
     live_cells_for_observable_step = live_cells_for_step[observation_step-1]
     return len(live_cells_for_observable_step)
 
 
-def time_scalar_analysis(output, _) -> float or None:
-    finish_condition = output['finishCondition']
-    if finish_condition != 'BORDER':
-        return None
+def time_scalar_analysis(output, _) -> float:
     live_cells_for_step = output['liveCellsForStep']
     return len(live_cells_for_step)
 
 
-def slope_scalar_analysis(output, observation_step) -> float or None:
+def size_slope_scalar_analysis(output, observation_step) -> float:
     live_cells_for_step = output['liveCellsForStep']
     if len(live_cells_for_step) <= observation_step:
-        return None
+        observation_step = len(live_cells_for_step)
     live_cells_initial_step = live_cells_for_step[0]
     live_cells_for_observable_step = live_cells_for_step[observation_step-1]
     y1 = len(live_cells_initial_step)
@@ -34,10 +31,48 @@ def slope_scalar_analysis(output, observation_step) -> float or None:
     return (y2 - y1) / observation_step
 
 
+def further_distance_to_center(domain, is_3d, live_cells) -> float:
+    center_x = (domain['x.max'] - domain['x.min']) / 2
+    center_y = (domain['y.max'] - domain['y.min']) / 2
+    center_z = (domain['z.max'] - domain['z.min']) / 2 if is_3d else 0
+    further_distance = 0
+    for live_cell in live_cells:
+        x = live_cell['x']
+        y = live_cell['y']
+        z = live_cell['z'] if is_3d else 0
+        distance = ((x - center_x) ** 2 + (y - center_y) ** 2 + (z - center_z) ** 2) ** 0.5
+        if distance > further_distance:
+            further_distance = distance
+    return further_distance
+
+
+def further_distance_scalar_analysis(output, observation_step) -> float:
+    domain = output['border']
+    is_3d = output['is3D']
+    live_cells_for_step = output['liveCellsForStep']
+    if len(live_cells_for_step) <= observation_step:
+        observation_step = len(live_cells_for_step)
+    live_cells_for_observable_step = live_cells_for_step[observation_step-1]
+    return further_distance_to_center(domain, is_3d, live_cells_for_observable_step)
+
+
+def further_distance_slope_scalar_analysis(output, _) -> float:
+    domain = output['border']
+    is_3d = output['is3D']
+    live_cells_for_step = output['liveCellsForStep']
+    live_cells_initial_step = live_cells_for_step[0]
+    live_cells_final_step = live_cells_for_step[-1]
+    y1 = further_distance_to_center(domain, is_3d, live_cells_initial_step)
+    y2 = further_distance_to_center(domain, is_3d, live_cells_final_step)
+    return (y2 - y1) / len(live_cells_for_step)
+
+
 mean_and_std_by_scalar_analysis = {
     'size': size_scalar_analysis,
     'time': time_scalar_analysis,
-    'slope': slope_scalar_analysis
+    'size_slope': size_slope_scalar_analysis,
+    'further_distance': further_distance_scalar_analysis,
+    'further_distance_slope': further_distance_slope_scalar_analysis
 }
 
 
@@ -45,7 +80,7 @@ def mean_and_std_fn(config):
     output_file_prefix = config['outputFilePrefix']
     input_directory = config['inputDirectory']
     scalar_analysis = config['scalarAnalysis']
-    decimal_precision = int(config['decimalPrecision'])
+    # decimal_precision = int(config['decimalPrecision'])
     observation_step = int(config['observationStep'])
 
     scalar_analysis_fn = mean_and_std_by_scalar_analysis[scalar_analysis]
@@ -62,10 +97,6 @@ def mean_and_std_fn(config):
             input = float(input) * 100
             scalar_value = scalar_analysis_fn(output_json, observation_step)
 
-            if scalar_value is None:
-                print(f"Output file {output} does not have data for observation step {observation_step}")
-                continue
-
             if input not in scalar_analysis_by_input:
                 scalar_analysis_by_input[input] = []
 
@@ -75,11 +106,11 @@ def mean_and_std_fn(config):
 
     for input, scalar_values in scalar_analysis_by_input.items():
         mean = statistics.mean(scalar_values)
-        real_mean = round(mean, decimal_precision)
+        # real_mean = round(mean, decimal_precision)
         std = statistics.stdev(scalar_values) if len(scalar_values) > 1 else 0
-        real_std = round(std, decimal_precision)
+        # real_std = round(std, decimal_precision)
 
-        scalar_mean_and_std_by_input[input] = {'mean': real_mean, 'std': real_std}
+        scalar_mean_and_std_by_input[input] = {'mean': mean, 'std': std}
 
     gmt = time.gmtime()
     timestamp = time.strftime('%Y-%m-%d_%H-%M-%S', gmt)
@@ -99,7 +130,9 @@ def mean_and_std_fn(config):
 ylabel_by_scalar_analysis = {
     'size': lambda observation_step: f'Tamaño de celulas vivas\ntras {observation_step} pasos (unidades)',
     'time': lambda _: f'Tiempo de ejecución hasta que las células\nvivas alcanzan el borde (pasos)',
-    'slope': lambda observation_step: f'Pendiente de crecimiento de células vivas\ntras {observation_step} pasos (unidades/paso)'
+    'size_slope': lambda observation_step: f'Pendiente de crecimiento de células vivas\ntras {observation_step} pasos (unidades/paso)',
+    'further_distance': lambda observation_step: f'Distancia más lejana al centro de las células vivas\ntras {observation_step} pasos (unidades)',
+    'further_distance_slope': lambda _: f'Pendiente de crecimiento de la distancia más lejana\nal centro de las células vivas (unidades/paso)'
 }
 
 
@@ -137,6 +170,7 @@ def plot_results(config):
     plt.figure(figsize=(10, 5))
     plt.xlabel("Porcentaje de células vivas respecto al dominio inicial (%)")
     plt.ylabel(ylabel_fn(observation_step))
+    plt.xticks(x)
     plt.errorbar(x, y, yerr=yerr, fmt='o-', capsize=5, capthick=2)
     plt.show()
 
