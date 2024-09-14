196c1ecd4f7ae1e6a3647eaaedc10e7887eeb7f2
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-09-14 15:47:01 -0300

    feat: animate circular plane and save as mp4

--- TP3/EventDrivenAnimation/animation_opencv.py
+++ TP3/EventDrivenAnimation/animation_opencv.py
@@ -3,40 +3,51 @@ import cv2
 import numpy as np
 
 # Load the JSON data
-with open('./input/dynamic.json', 'r') as file:
+with open('../EventDrivenSimulation/output/example/2024-09-14_15-44-50/dynamic.json', 'r') as file:
     dynamic_data = json.load(file)
 
-with open('./input/static.json', 'r') as file:
+with open('../EventDrivenSimulation/output/example/2024-09-14_15-44-50/static.json', 'r') as file:
     static_data = json.load(file)
 
 # Extract simulation parameters
 L = static_data['inputData']['L']
-obstacle = static_data['inputData']['obstacles'][0]
+simulation_type = static_data['inputData']['simulationType']
+plane_type = static_data['inputData']['planeType']
 
 # Set up window size based on the size of the simulation plane
-window_size = 600
-scale = window_size / L
-
-# Obstacle parameters
-obstacle_x = int(obstacle['position']['x'] * scale)
-obstacle_y = int(obstacle['position']['y'] * scale)
-obstacle_radius = int(obstacle['radius'] * scale)
+plane_size = 600
+window_size = plane_size + 50
+scale = plane_size / L
 
 
 # Create a function to draw a frame
 def draw_frame(particles, frame_number):
+    adj_y = 0
+    adj_x = 0
+
     # Create a blank white image
     frame = np.ones((window_size, window_size, 3), dtype=np.uint8) * 255
 
-    # Draw the obstacle (red circle)
-    cv2.circle(frame, (obstacle_x, obstacle_y), obstacle_radius, (0, 0, 255), -1)
+    # Draw circular plane if the plane type is 'CIRCULAR' and set (0,0) as the center
+    if plane_type == 'CIRCULAR':
+        adj_y = window_size // 2
+        adj_x = window_size // 2
+        cv2.circle(frame, (adj_x, adj_y), plane_size // 2, (0, 0, 0), 2)
+
+    # Draw the obstacle (red circle) if the simulation type is 'OBSTACLE'
+    if simulation_type == 'OBSTACLE':
+        obstacle = static_data['inputData']['obstacles'][0]
+        obstacle_x = int(obstacle['position']['x'] * scale)
+        obstacle_y = int(obstacle['position']['y'] * scale)
+        obstacle_radius = int(obstacle['radius'] * scale)
+        cv2.circle(frame, (obstacle_x + adj_x, obstacle_y + adj_y), obstacle_radius, (0, 0, 255), -1)
 
     # Draw each particle (blue circle)
     for particle in particles:
         x = int(particle['position']['x'] * scale)
         y = int(particle['position']['y'] * scale)
         particle_radius = int(particle['radius'] * scale)
-        cv2.circle(frame, (x, y), particle_radius, (255, 0, 0), -1)
+        cv2.circle(frame, (x + adj_x, y + adj_y), particle_radius, (255, 0, 0), -1)
 
     # Add text to show the frame number
     cv2.putText(frame, f"Frame: {frame_number}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
@@ -44,19 +55,30 @@ def draw_frame(particles, frame_number):
     return frame
 
 
+fourcc = cv2.VideoWriter.fourcc(*'mp4v')
+out = cv2.VideoWriter('particle_simulation.mp4', fourcc, 10.0, (window_size, window_size))
+show_frame = True
+
 # Get particle data from events and render each frame
+events = dynamic_data['events']
+length = len(events)
 for i, event in enumerate(dynamic_data['events']):
     particles = event['particles']
 
     # Draw the frame
     frame = draw_frame(particles, i + 1)
 
-    # Show the frame using OpenCV
-    cv2.imshow('Particle Simulation', frame)
+    # Write the frame to the video file
+    out.write(frame)
+    print(f"Frame {i + 1}/{length} rendered")
+
+    if show_frame:
+        cv2.imshow('Particle Simulation', frame)
 
     # Wait for a short moment to simulate the animation speed
     if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+        show_frame = False
+        cv2.destroyAllWindows()
 
 # When the animation is done, close the window
-cv2.destroyAllWindows()
+out.release()
