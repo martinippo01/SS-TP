f7bbf78220f8a813489888b43aa3e230154c6b5a
mauritobaez <mauroleanbaez@gmail.com>
2024-09-18 00:23:15 -0300

    Ahora si 1.3

--- TP3/EventDrivenAnimation/crash_vs_time/crashing_particles.py
+++ TP3/EventDrivenAnimation/crash_vs_time/crashing_particles.py
@@ -3,12 +3,13 @@ from matplotlib import pyplot as plt
 import os
 
 # Para el 1.3a poner en True, para el 1.3b poner en False
-each_particle_bounces_once = False
+each_particle_bounces_once = True
 
 # Specify the parent directory path
-parent_directory = '../../EventDrivenSimulation/output/para1punto3_3'
+parent_directory = '../../EventDrivenSimulation/output/para1punto3_1'
 # Si output tiene directorios dentro que a su vez tienen los distintos directorios con timestamp, poner True
 multiple_directories = False
+
 
 def add_plot(dynamic_file):
     with open(dynamic_file, 'r') as file:
@@ -32,25 +33,25 @@ def add_plot(dynamic_file):
             current_amount += 1
             cum_amount.append(current_amount)
             t.append(crash["tc"])
-
+        print(len(cum_amount))
         plt.plot(t, cum_amount)
 
 
-def go_one_directory_deeper(parent):
+def go_one_directory_deeper(parent, func):
     for data_directory in os.listdir(parent):
         data_path = os.path.join(parent, data_directory)
 
         if os.path.isdir(data_path):
-            retrieve_data(data_path)
+            retrieve_data(data_path, func)
 
 
-def retrieve_data(data_path):
+def retrieve_data(data_path, func):
     specific_file = 'dynamic.json'
     specific_file_path = os.path.join(data_path, specific_file)
 
     # Check if the specific file exists and perform some action
     if os.path.isfile(specific_file_path):
-        add_plot(specific_file_path)
+        func(specific_file_path)
 
 
 for item in os.listdir(parent_directory):
@@ -58,8 +59,8 @@ for item in os.listdir(parent_directory):
     print(item)
     if os.path.isdir(item_path):
         if multiple_directories:
-            go_one_directory_deeper(item_path)
+            go_one_directory_deeper(item_path, add_plot)
         else:
-            retrieve_data(item_path)
+            retrieve_data(item_path, add_plot)
 
 plt.show()
