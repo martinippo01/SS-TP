01ef213d38cfc39271b36c7b0f9a5ccdaf1346ad
Axel Preiti Tasat <axel.preitit@gmail.com>
2024-10-05 14:35:42 -0300

    fix: CoupledVerlet

--- TP4/TimeBasedSimulation/src/main/java/ar/edu/itba/ss/coupled/CoupledVerletAlgorithm.java
+++ TP4/TimeBasedSimulation/src/main/java/ar/edu/itba/ss/coupled/CoupledVerletAlgorithm.java
@@ -1,16 +1,15 @@
 package ar.edu.itba.ss.coupled;
 
 import ar.edu.itba.ss.Particle;
-import ar.edu.itba.ss.Position;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.function.BiFunction;
 
 public class CoupledVerletAlgorithm extends CoupledAlgorithm {
 
-    private final List<Double> previousRs;
+    private List<Double> previousRs;
 
     public CoupledVerletAlgorithm(double k, double a, double w, int n, List<Particle> particles) {
         super(k, a, w, n, particles);
@@ -18,35 +17,36 @@ public class CoupledVerletAlgorithm extends CoupledAlgorithm {
     }
 
     private List<Double> getPhantomRs(double dt) {
-//        List<Particle> particles = getParticles();
-//        List<Double> phantomRs = new ArrayList<>();
-//        BiFunction<Particle, Double, Double> getPhantomR = (particle, force) -> {
-//            double r0 = particle.getPosition().getY();
-//            double v0 = particle.getVelocity().getY();
-//            double mass = particle.getMass();
-//            double phantomV = v0 - (dt/mass)*force;
-//            return r0 - phantomV * dt + dt * dt * force / (2 * mass);
-//        };
-//
-//        double firstForce = getFirstForce();
-//        double firstPhantomR = getPhantomR.apply(particles.get(0), firstForce);
-//        phantomRs.add(firstPhantomR);
-//
-//        for (int i = 1; i < getN()-1; i++) {
-//            double force = getIntermediateForce(i);
-//            double currentPhantomR = getPhantomR.apply(particles.get(i), force);
-//            phantomRs.add(currentPhantomR);
-//        }
-//        return phantomRs;
+        List<Particle> particles = getParticles();
+        List<Double> phantomRs = new ArrayList<>();
+        BiFunction<Particle, Double, Double> getPhantomR = (particle, force) -> {
+            double r0 = particle.getPosition().getY();
+            double v0 = particle.getVelocity().getY();
+            double mass = particle.getMass();
+            double phantomV = v0 - (dt/mass)*force;
+            return r0 - phantomV * dt + dt * dt * force / (2 * mass);
+        };
 
-        return getParticles().stream()
-                .map(Particle::getPosition)
-                .map(Position::getY)
-                .collect(Collectors.toCollection(ArrayList::new));
+        double firstForce = getFirstForce();
+        double firstPhantomR = getPhantomR.apply(particles.get(0), firstForce);
+        phantomRs.add(firstPhantomR);
+
+        for (int i = 1; i < getN()-1; i++) {
+            double force = getIntermediateForce(i);
+            double currentPhantomR = getPhantomR.apply(particles.get(i), force);
+            phantomRs.add(currentPhantomR);
+        }
+        this.previousRs = phantomRs;
+        return phantomRs;
+
+//        return getParticles().stream()
+//                .map(Particle::getPosition)
+//                .map(Position::getY)
+//                .collect(Collectors.toCollection(ArrayList::new));
     }
 
     private void updateParticle(int i, double force, double dt) {
-        List<Double> previousRs = Optional.ofNullable(this.previousRs).orElseGet(() -> getPhantomRs(dt));
+        this.previousRs = Optional.ofNullable(this.previousRs).orElseGet(() -> getPhantomRs(dt));
         Particle current = getParticles().get(i);
         double currentR = current.getPosition().getY();
         double previousR = previousRs.get(i);
@@ -59,17 +59,22 @@ public class CoupledVerletAlgorithm extends CoupledAlgorithm {
 
     @Override
     public void evolve(double dt) {
-        // First particle
-        double firstForce = getFirstForce();
-        updateParticle(0, firstForce, dt);
+        List<Double> newForces = new ArrayList<>();
 
-        // Intermediate particles
-        for(int i = 1; i < getN()-1; i++) {
-            double force = getIntermediateForce(i);
-            updateParticle(i, force, dt);
+        // Calculate first particle force
+        newForces.add(getFirstForce());
+
+        // Calculate intermediate forces
+        for (int i = 1; i < getN()-1; i++) {
+            newForces.add(getIntermediateForce(i));
         }
 
-        // Last particle
+        // Update first n-1 particles with new forces
+        for (int i = 0; i < getN()-1; i++) {
+            updateParticle(i, newForces.get(i), dt);
+        }
+
+        // Update last particle
         updateLastParticle(dt);
     }
 }
