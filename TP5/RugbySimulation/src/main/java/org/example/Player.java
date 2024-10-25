66a7451af93cee39ca46f927fd0959c0eb52e536
mauritobaez <mauroleanbaez@gmail.com>
2024-10-25 10:54:17 -0300

    ...

---
+++ TP5/RugbySimulation/src/main/java/org/example/Player.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public abstract class Player {
+    final private int id;
+    private Position pos;
+    private Velocity vel;
+    private double radious;
+
+    public Player(int id, Position pos, Velocity vel, double radious) {
+        this.id = id;
+        this.pos = pos;
+        this.vel = vel;
+        this.radious = radious;
+    }
+}
---
+++ TP5/RugbySimulation/src/main/java/org/example/Velocity.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Velocity {
+
+    private double vX;
+    private double vY;
+
+    public Velocity(double vX, double vY) {
+        this.vX = vX;
+        this.vY = vY;
+    }
+
+    public void setVX(double vX) {
+        this.vX = vX;
+    }
+
+    public void setVY(double vY) {
+        this.vY = vY;
+    }
+
+    public double getVX() {
+        return vX;
+    }
+
+    public double getVY() {
+        return vY;
+    }
+}
